33818bdc34e681f7ae5ad03cb869e79e8590a13a
LincolnLidanya <lincolnlidanya@gmail.com>
2020-06-01 20:32:17 +0300

    Add Zeller function for calculating the Day of the week

--- assignment2.js
+++ assignment2.js
@@ -36,3 +36,43 @@ function myFunction() {
         alert("You name is : " + name);
     }
 }
+
+//Zeller function that return the day of the week
+function Zeller(D, M, Y) {
+    var Day = "";
+
+    if (M < 3) {
+        M = M + 12;
+        Y = Y - 1;
+    }
+
+    var C = Math.floor(Y / 100);
+    var K = Y - (100 * C);
+
+    var S = Math.floor(2.6 * M - 5.39) + Math.floor(K / 4) + Math.floor(C / 4) + D + K - (2 * C);
+
+    var ans = S - (7 * Math.floor(S / 7));
+
+    if (ans == 0) {
+        Day = "Sunday";
+    }
+    else if (ans == 1) {
+        Day = "Monday";
+    }
+    else if (ans == 2) {
+        Day = "Tuesday";
+    }
+    else if (ans == 3) {
+        Day = "Wednesday";
+    }
+    else if (ans == 4) {
+        Day = "Thursday";
+    }
+    else if (ans == 5) {
+        Day = "Friday";
+    }
+    else {
+        Day = "Saturday";
+    }
+    return Day;
+}
