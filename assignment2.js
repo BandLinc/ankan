ebb61dc8e73826ebd12c169a3362e997e3c07313
LincolnLidanya <lincolnlidanya@gmail.com>
2020-06-01 20:30:20 +0300

    Add myFunction

--- assignment2.js
+++ assignment2.js
@@ -1,3 +1,4 @@
+//Datepicker function for jquery
 $("#datepicker").datepicker();
 $(document).ready(function () {
     var age = "";
@@ -12,3 +13,26 @@ $(document).ready(function () {
         yearRange: '1970:2020'
     })
 })
+
+//Function for validating date 
+//Function call zeller method
+//Functions call ankan function
+function myFunction() {
+
+    var dob = document.getElementById("datepicker").value;
+
+    var gender = document.getElementById("gender").value;
+
+    if (dob == "" || gender == "") {
+        alert("Sorry, you missed to enter some information");
+    } else {
+        var d = new Date(dob);
+        var date = d.getDate();
+        var month = d.getMonth() + 1;
+        var year = d.getFullYear();
+        var day = Zeller(date, month, year);
+        var calculator=[day,gender];
+        var name=ankan(calculator);
+        alert("You name is : " + name);
+    }
+}
